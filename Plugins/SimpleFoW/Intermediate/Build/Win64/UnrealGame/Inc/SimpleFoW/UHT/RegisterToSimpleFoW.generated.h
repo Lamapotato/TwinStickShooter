f08b409c173375e5957d10ce4b19beab63b21cf0
Lamapotato <agranat42@gmail.com>
2026-05-27 21:49:36 +0300

    27. Финальный билд проекта и запуск на таргетируемой платформе

---
+++ Plugins/SimpleFoW/Intermediate/Build/Win64/UnrealGame/Inc/SimpleFoW/UHT/RegisterToSimpleFoW.generated.h
@@ -0,0 +1,61 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "RegisterToSimpleFoW.h"
+
+#ifdef SIMPLEFOW_RegisterToSimpleFoW_generated_h
+#error "RegisterToSimpleFoW.generated.h already included, missing '#pragma once' in RegisterToSimpleFoW.h"
+#endif
+#define SIMPLEFOW_RegisterToSimpleFoW_generated_h
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+// ********** Begin Class URegisterToSimpleFoW *****************************************************
+struct Z_Construct_UClass_URegisterToSimpleFoW_Statics;
+SIMPLEFOW_API UClass* Z_Construct_UClass_URegisterToSimpleFoW_NoRegister();
+
+#define FID_git_TwinStickShooter_Plugins_SimpleFoW_Source_SimpleFoW_Public_RegisterToSimpleFoW_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesURegisterToSimpleFoW(); \
+	friend struct ::Z_Construct_UClass_URegisterToSimpleFoW_Statics; \
+	static UClass* GetPrivateStaticClass(); \
+	friend SIMPLEFOW_API UClass* ::Z_Construct_UClass_URegisterToSimpleFoW_NoRegister(); \
+public: \
+	DECLARE_CLASS2(URegisterToSimpleFoW, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/SimpleFoW"), Z_Construct_UClass_URegisterToSimpleFoW_NoRegister) \
+	DECLARE_SERIALIZER(URegisterToSimpleFoW)
+
+
+#define FID_git_TwinStickShooter_Plugins_SimpleFoW_Source_SimpleFoW_Public_RegisterToSimpleFoW_h_13_ENHANCED_CONSTRUCTORS \
+	/** Deleted move- and copy-constructors, should never be used */ \
+	URegisterToSimpleFoW(URegisterToSimpleFoW&&) = delete; \
+	URegisterToSimpleFoW(const URegisterToSimpleFoW&) = delete; \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, URegisterToSimpleFoW); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(URegisterToSimpleFoW); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(URegisterToSimpleFoW) \
+	NO_API virtual ~URegisterToSimpleFoW();
+
+
+#define FID_git_TwinStickShooter_Plugins_SimpleFoW_Source_SimpleFoW_Public_RegisterToSimpleFoW_h_10_PROLOG
+#define FID_git_TwinStickShooter_Plugins_SimpleFoW_Source_SimpleFoW_Public_RegisterToSimpleFoW_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_git_TwinStickShooter_Plugins_SimpleFoW_Source_SimpleFoW_Public_RegisterToSimpleFoW_h_13_INCLASS_NO_PURE_DECLS \
+	FID_git_TwinStickShooter_Plugins_SimpleFoW_Source_SimpleFoW_Public_RegisterToSimpleFoW_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+class URegisterToSimpleFoW;
+
+// ********** End Class URegisterToSimpleFoW *******************************************************
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_git_TwinStickShooter_Plugins_SimpleFoW_Source_SimpleFoW_Public_RegisterToSimpleFoW_h
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
